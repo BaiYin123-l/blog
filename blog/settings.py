4fa4599fa25400483947290559f1136ec568e218
白影 <356741418@qq.com>
2025-06-14 16:16:30 +0800

    更新 .gitignore 文件，添加 .DS_Store 和 .cursorindexingignore，修复 manage.py 中的执行命令行语句格式

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nucdth+r0$5lk7dww%*00s1pihu0&dn!4e3$%6aq9b)d5vh)8s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'bootstrap-5.3.6-dist',  # Assuming you have a 'bootstrap' directory for Bootstrap files
+    BASE_DIR / 'bootstrap-icons-1.13.1' # Assuming you have a 'bootstrap-icons' directory for Bootstrap Icons files
+]
+STATIC_ROOT = 'statics/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

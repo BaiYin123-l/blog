02641d2842abcb44d09164289cb212a2f4858bec
白影 <356741418@qq.com>
2025-06-15 10:38:14 +0800

    feat: add license-checker dependency and new styles for about page
    - Added `license-checker` as a dependency in package.json.
    - Created a new CSS file `about.css` for styling the about page.
    - Added a new JSON file `node-depends.json` to track node dependencies and their licenses.
    - Created a new empty JSON file `python-depends.json` for future use.
    - Updated `about.html` to include a table displaying Python and Node.js dependencies with their details.
    - Modified `base.html` to change the language to Simplified Chinese and improved the layout of the header and footer.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def blogs(request):
+    
     return render(request, "blogs.html")
 
 def about(request):
-    return render(request, "about.html")
+    with open(r"static\python-depends.json",encoding='utf-16 le') as f:
+        content = f.read()
+        # 检查并移除 BOM
+        if content.startswith('\ufeff'):
+            content = content[1:]
+        t1 = json.loads(content)
+    with open(r"static\node-depends.json", encoding='utf-8') as f:
+        content = f.read()
+        # 检查并移除 BOM
+        if content.startswith('\ufeff'):
+            content = content[1:]
+        t2 = json.loads(content)
+    return render(request, "about.html", {'t1':t1, 't2':t2})

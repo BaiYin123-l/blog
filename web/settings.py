2f9a4ddd17f346d77ec4fede21b6c929deb30dbb
白影 <356741418@qq.com>
2025-06-15 23:58:01 +0800

    feat: 更新静态文件目录配置，修正 Marked 库的路径

--- web/settings.py
+++ web/settings.py
@@ -122,9 +122,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'node_modules' / 'bootstrap' / 'dist',
     BASE_DIR / 'node_modules' / 'bootstrap-icons',
-    BASE_DIR / 'node_modules' / 'marked',
+    BASE_DIR / 'node_modules' / 'marked' ,
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
